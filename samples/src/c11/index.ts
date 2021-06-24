ffa3acbf0a60c38e94a9ccbbd839b6e016e7b36d
Sri Panyam <sri.panyam@gmail.com>
2021-06-24 00:54:07 -0700

    Updating to latest tlex accounting for multiline handling in flex RE parsing

--- samples/src/c11/index.ts
+++ samples/src/c11/index.ts
@@ -1,5 +1,32 @@
+import * as TSU from "@panyam/tsutils";
+import * as TLEX from "tlex";
 import * as G from "galore";
 export * from "./data";
 import { GRAMMAR } from "./data";
 
-export const newParser = G.newLRParser.bind(null, GRAMMAR);
+export function newParser(params: any): [G.LR.Parser, TLEX.NextTokenFunc | null] {
+  const [parser, tokenizer] = G.newLRParser(GRAMMAR, params);
+  /*
+  const typedefNames = new Set<string>();
+  const enumNames = new Set<string>();
+  parser.onNextToken = (token: TLEX.Token) => {
+    if (token.tag == "IDENTIFIER") {
+      if (typedefNames.has(token.value)) {
+        token.tag = "TYPEDEF_NAME";
+      } else if (enumNames.has(token.value)) {
+        token.tag = "ENUMERATION_CONSTANT";
+      }
+    }
+    // Check token and extract enum or typedef names
+    return token;
+  };
+
+  parser.onReduction = (node: G.PTNode, rule: G.Rule) => {
+    if (node.sym.label == "declaration" && node.childAt(0).sym.label == "declaration_specifiers") {
+      return node;
+    }
+    return node;
+  };
+  */
+  return [parser, tokenizer];
+}
--- samples/src/c11/tests/t1.spec.ts
+++ samples/src/c11/tests/t1.spec.ts
@@ -1,23 +1,22 @@
 const util = require("util");
-import { PTNode } from "../../../../src/parser";
-import { newLRParser } from "../../../../src/factory";
-import { GRAMMAR } from "../data";
+import { newParser } from "../";
 const fs = require("fs");
+const yaml = require("js-yaml");
 
 function readFile(fname: string): string {
   return fs.readFileSync(__dirname + "/" + fname, "utf8");
 }
 
-function parseFile(fname: string, contents?: string, debug = false): null | PTNode {
+function parseFile(fname: string, contents?: string, debug = false): any {
   if (!contents) contents = readFile(fname);
-  const [parser, _] = newLRParser(GRAMMAR, { type: "slr", debug: debug });
+  const [parser, _] = newParser({ type: "slr", debug: debug });
   const t1 = Date.now();
   const result = parser.parse(contents) || null;
   const t2 = Date.now();
   console.log(`'${fname}' parsed in ${t2 - t1} ms`);
   if (debug) {
     console.log(
-      `Parse Result: ${util.inspect(result?.debugValue(), {
+      `Parse Result: \n${util.inspect(yaml.dump(result?.debugValue()), {
         showHidden: false,
         depth: null,
         maxArrayLength: null,
@@ -31,56 +30,36 @@ function parseFile(fname: string, contents?: string, debug = false): null | PTNo
 describe("C11 Parser", () => {
   test("1", () => {
     const result = parseFile("Case 1", ` int main(void) { } `);
-    expect(result?.debugValue()).toEqual([
-      "translation_unit",
-      [
-        [
-          "external_declaration",
-          [
-            [
-              "function_definition",
-              [
-                ["declaration_specifiers", [["type_specifier", [["INT", "int"]]]]],
-                [
-                  "declarator",
-                  [
-                    [
-                      "direct_declarator",
-                      [
-                        ["direct_declarator", [["IDENTIFIER", "main"]]],
-                        ['"("', "("],
-                        [
-                          "parameter_type_list",
-                          [
-                            [
-                              "parameter_list",
-                              [
-                                [
-                                  "parameter_declaration",
-                                  [["declaration_specifiers", [["type_specifier", [["VOID", "void"]]]]]],
-                                ],
-                              ],
-                            ],
-                          ],
-                        ],
-                        ['")"', ")"],
-                      ],
-                    ],
-                  ],
-                ],
-                [
-                  "compound_statement",
-                  [
-                    ['"{"', "{"],
-                    ['"}"', "}"],
-                  ],
-                ],
-              ],
-            ],
-          ],
-        ],
-      ],
-    ]);
+    expect(yaml.dump(result?.debugValue())).toEqual(
+      "- translation_unit\n" +
+        "- - - external_declaration\n" +
+        "    - - - function_definition\n" +
+        "        - - - declaration_specifiers\n" +
+        "            - - - type_specifier\n" +
+        "                - - - INT\n" +
+        "                    - int\n" +
+        "          - - declarator\n" +
+        "            - - - direct_declarator\n" +
+        "                - - - direct_declarator\n" +
+        "                    - - - IDENTIFIER\n" +
+        "                        - main\n" +
+        `                  - - '"("'\n` +
+        "                    - (\n" +
+        "                  - - parameter_type_list\n" +
+        "                    - - - parameter_list\n" +
+        "                        - - - parameter_declaration\n" +
+        "                            - - - declaration_specifiers\n" +
+        "                                - - - type_specifier\n" +
+        "                                    - - - VOID\n" +
+        "                                        - void\n" +
+        `                  - - '")"'\n` +
+        "                    - )\n" +
+        "          - - compound_statement\n" +
+        `            - - - '"{"'\n` +
+        "                - '{'\n" +
+        `              - - '"}"'\n` +
+        "                - '}'\n",
+    );
   });
 
   test("2", () => {
@@ -89,6 +68,100 @@ describe("C11 Parser", () => {
       `typedef int T;
       void f(void) {
         T * b;
+      }`,
+    );
+    expect(yaml.dump(result?.debugValue())).toEqual(
+      "- translation_unit\n" +
+        "- - - translation_unit\n" +
+        "    - - - external_declaration\n" +
+        "        - - - declaration\n" +
+        "            - - - declaration_specifiers\n" +
+        "                - - - storage_class_specifier\n" +
+        "                    - - - TYPEDEF\n" +
+        "                        - typedef\n" +
+        "                  - - declaration_specifiers\n" +
+        "                    - - - type_specifier\n" +
+        "                        - - - INT\n" +
+        "                            - int\n" +
+        "              - - init_declarator_list\n" +
+        "                - - - init_declarator\n" +
+        "                    - - - declarator\n" +
+        "                        - - - direct_declarator\n" +
+        "                            - - - IDENTIFIER\n" +
+        "                                - T\n" +
+        `              - - '";"'\n` +
+        "                - ;\n" +
+        "  - - external_declaration\n" +
+        "    - - - function_definition\n" +
+        "        - - - declaration_specifiers\n" +
+        "            - - - type_specifier\n" +
+        "                - - - VOID\n" +
+        "                    - void\n" +
+        "          - - declarator\n" +
+        "            - - - direct_declarator\n" +
+        "                - - - direct_declarator\n" +
+        "                    - - - IDENTIFIER\n" +
+        "                        - f\n" +
+        `                  - - '"("'\n` +
+        "                    - (\n" +
+        "                  - - parameter_type_list\n" +
+        "                    - - - parameter_list\n" +
+        "                        - - - parameter_declaration\n" +
+        "                            - - - declaration_specifiers\n" +
+        "                                - - - type_specifier\n" +
+        "                                    - - - VOID\n" +
+        "                                        - void\n" +
+        `                  - - '")"'\n` +
+        "                    - )\n" +
+        "          - - compound_statement\n" +
+        `            - - - '"{"'\n` +
+        "                - '{'\n" +
+        "              - - block_item_list\n" +
+        "                - - - block_item\n" +
+        "                    - - - statement\n" +
+        "                        - - - expression_statement\n" +
+        "                            - - - expression\n" +
+        "                                - - - assignment_expression\n" +
+        "                                    - - - conditional_expression\n" +
+        "                                        - - - logical_or_expression\n" +
+        "                                            - - - logical_and_expression\n" +
+        "                                                - - - inclusive_or_expression\n" +
+        "                                                    - - - exclusive_or_expression\n" +
+        "                                                        - - - and_expression\n" +
+        "                                                            - - - equality_expression\n" +
+        "                                                                - - - relational_expression\n" +
+        "                                                                    - - - shift_expression\n" +
+        "                                                                        - - - additive_expression\n" +
+        "                                                                            - - - multiplicative_expression\n" +
+        "                                                                                - - - multiplicative_expression\n" +
+        "                                                                                    - - - cast_expression\n" +
+        "                                                                                        - - - unary_expression\n" +
+        "                                                                                            - - - postfix_expression\n" +
+        "                                                                                                - - - primary_expression\n" +
+        "                                                                                                    - - - IDENTIFIER\n" +
+        "                                                                                                        - T\n" +
+        `                                                                                  - - '"*"'\n` +
+        "                                                                                    - '*'\n" +
+        "                                                                                  - - cast_expression\n" +
+        "                                                                                    - - - unary_expression\n" +
+        "                                                                                        - - - postfix_expression\n" +
+        "                                                                                            - - - primary_expression\n" +
+        "                                                                                                - - - IDENTIFIER\n" +
+        "                                                                                                    - b\n" +
+        `                              - - '";"'\n` +
+        "                                - ;\n" +
+        `              - - '"}"'\n` +
+        "                - '}'\n",
+    );
+  });
+  test("3", () => {
+    const result = parseFile(
+      "Case 3",
+      `
+      // variable_star.c
+      int T, b;
+      void f(void) {
+      T * b;
       }`,
       true,
     );

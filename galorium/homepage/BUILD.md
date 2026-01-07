5436dc92f33cd6740b00a66be93577f94eae4b99
Sri Panyam <sri.panyam@gmail.com>
2026-01-06 23:36:51 -0800

    Galorium no longer needed due to solid docs

--- galorium/homepage/BUILD.md
+++
@@ -1,9 +0,0 @@
-
-Documentation BUILD Instructions:
-
-```
-cd homepage
-git submodule update --init --recursive
-sudo npm install -g autoprefixer
-sudo npm install -g postcss-cli
-```
--- galorium/hp/src/components/playground/events.ts
+++
@@ -1,6 +0,0 @@
-
-export const ParserCompiled = "ParserCompiled";
-export const InputParsed = "InputParsed";
-export const GrammarParsed = "GrammarParsed";
-export const GrammarSelected = "GrammarSelected";
-export const Log = "Log";
